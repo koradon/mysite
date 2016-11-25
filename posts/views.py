86279561909c2b71ec9656735d7887a4302213f8
koradon <michal.sipa@gmail.com>
2016-11-25 11:21:57 +0100

    Added form for creating and updating posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -11,7 +12,17 @@ def index(request):
 
 
 def post_create(request):
-    return HttpResponse("create")
+    form = PostForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 
 def post_detail(request, id=None):
@@ -32,8 +43,22 @@ def post_list(request):
     return render(request, "index.html", context)
 
 
-def post_update(request):
-    return HttpResponse("update")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+
+    form = PostForm(request.POST or None, instance=instance)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form
+    }
+    return render(request, "post_form.html", context)
 
 
 def post_delete(request):
