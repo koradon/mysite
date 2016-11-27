61e969d44d5f6682e1bf8707fb67d03ab709c60a
koradon <michal.sipa@gmail.com>
2016-11-27 13:43:14 +0100

    Added messages. First template inheritance

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
@@ -17,7 +18,10 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Succesfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "Error. Form is not valid")
 
     context = {
         "form": form,
@@ -51,6 +55,7 @@ def post_update(request, id=None):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Succesfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
@@ -61,5 +66,8 @@ def post_update(request, id=None):
     return render(request, "post_form.html", context)
 
 
-def post_delete(request):
-    return HttpResponse("delete")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully deleted")
+    return redirect("posts:list")
