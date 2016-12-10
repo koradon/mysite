416f6083d7ad0ae6114379de03c5a43a3cc55135
koradon <michal.sipa@gmail.com>
2016-12-10 14:13:02 +0100

    Paggination of the posts list.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import PostForm
 from .models import Post
@@ -37,10 +38,24 @@ def post_detail(request, id=None):
 
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+
+    paginator = Paginator(queryset_list, 15)
+
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         "title": "Detail",
         "objects_list": queryset,
+        "page_request_var": page_request_var
     }
     return render(request, "post_list.html", context)
 
