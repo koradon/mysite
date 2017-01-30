28574c2670b2f8f08d4bdfbb72277a9e670e3ffc
Michael <michal.sipa@gmail.com>
2017-01-30 18:49:27 +0100

    Added Facebook comments and like buttons. Added urlify to construct urls.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from urllib.parse import quote_plus
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -15,11 +15,14 @@ def index(request):
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None,
                     request.FILES or None)
 
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Succesfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -65,6 +68,8 @@ def post_list(request):
 
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
 
     form = PostForm(request.POST or None,
@@ -86,6 +91,8 @@ def post_update(request, slug=None):
 
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully deleted")
