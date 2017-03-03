5470c873950b2a952d21fe8cecce84799a0f7111
Michael <michal.sipa@gmail.com>
2017-03-03 14:00:37 +0100

    Now comments can be add as a replys to a comment. Comments are dynamicaly created by type of commented object.

--- comments/api/views.py
+++ comments/api/views.py
@@ -11,39 +11,26 @@ from posts.api.pagination import PostLimitOffsetPagination, PostPageNumberPagina
 
 from comments.models import Comment
 
-from .serializers import CommentSerializer, CommentDetailSerializer
+from .serializers import CommentSerializer, CommentDetailSerializer, create_comment_serializer
 
 
-# class PostCreateAPIView(CreateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostCreateUpdateSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
+class CommentCreateAPIView(CreateAPIView):
+    queryset = Comment.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        model_type = self.request.GET.get('type')
+        slug = self.request.GET.get('slug')
+        parent_id = self.request.GET.get('parent_id', None)
+        return create_comment_serializer(model_type=model_type,
+                                         slug=slug,
+                                         parent_id=parent_id,
+                                         user=self.request.user)
 
 
 class CommentDetailAPIView(RetrieveAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentDetailSerializer
-    # lookup_field = 'slug'
-
-
-# class PostUpdateAPIView(RetrieveUpdateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostCreateUpdateSerializer
-#     lookup_field = 'slug'
-#     permission_classes = [IsOwnerOrReadOnly]
-#
-#     def perform_update(self, serializer):
-#         serializer.save(user=self.request.user)
-
-
-# class PostDeleteAPIView(DestroyAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostDetailSerializer
-#     lookup_field = 'slug'
-#     permission_classes = [IsOwnerOrReadOnly, IsAdminUser]
 
 
 class CommentListAPIView(ListAPIView):
