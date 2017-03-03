d2cfee4871f7b2befd220668475411d231c8e2f8
Michael <michal.sipa@gmail.com>
2017-03-03 10:48:16 +0100

    Added new fields in serializers for post detail and post lists.

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,7 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField, SerializerMethodField
 
 from posts.models import Post
 
+
+post_detail_url = HyperlinkedIdentityField(
+        view_name="posts-api:detail",
+        lookup_field="slug",
+    )
+
+delete_url = HyperlinkedIdentityField(
+        view_name="posts-api:delete",
+        lookup_field="slug",
+    )
 
 class PostCreateUpdateSerializer(ModelSerializer):
     class Meta:
@@ -14,25 +24,65 @@ class PostCreateUpdateSerializer(ModelSerializer):
 
 
 class PostListSerializer(ModelSerializer):
+    url = post_detail_url
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+
     class Meta:
         model = Post
         fields = [
             'id',
+            'url',
+            'user',
+            'title',
+            'content',
+            'publish',
+            'image',
+        ]
+
+    def get_user(self, obj):
+        return str("{username}".format(username=obj.user.username))
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+
+        return image
+
+
+class PostDetailSerializer(ModelSerializer):
+    url = post_detail_url
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+    html = SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'url',
             'user',
             'title',
             'slug',
             'content',
-            'publish',
-        ]
-
-
-class PostDetailSerializer(ModelSerializer):
-    class Meta:
-        model = Post
-        fields = [
-            'id',
-            'title',
-            'slug',
+            'html',
             'content',
             'publish',
+            'image'
         ]
+
+    def get_html(self, obj):
+        return obj.get_markdown()
+
+    def get_user(self, obj):
+        return str("{username}".format(username=obj.user.username))
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+
+        return image
