aaea9ff9ecbed5fb7cc4bb6ea7b1fcfb4189c92d
Michael <michal.sipa@gmail.com>
2017-03-23 16:16:53 +0100

    removed unused tag "get_most_commented_posts"

--- posts/templatetags/posts_tags.py
+++ posts/templatetags/posts_tags.py
@@ -15,13 +15,3 @@ def total_posts():
 def show_latest_posts(count=3):
     latest_posts = Post.objects.active()[:count]
     return {"latest_posts": latest_posts}
-
-
-@register.assignment_tag
-def get_most_commented_posts():
-    most_commented_posts = Post.objects.active()\
-               .annotate(total_comments=Count(Post.comments))\
-               .order_by('-total_comments')[:3]
-    print(most_commented_posts)
-
-    return most_commented_posts
