b9cca4d252c295fb224215bb0e0cf8df5200ec59
Michael <michal.sipa@gmail.com>
2017-03-23 16:57:09 +0100

    Added RSS feed. Feed and sitemap does not work. Investigation is needed.

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
 from . import views
+from .feeds import LatestPostFeed
 
 
 urlpatterns = [
     #url(r'^$', views.index, name="index"),
     url(r'^$', views.post_list, name="list"),
+    url(r'^feed/$', LatestPostFeed(), name='post_feed'),
     url(r'^create/$', views.post_create, name="create"),
     url(r'^(?P<slug>[\w-]+)/$', views.post_detail, name="detail"),
     url(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name="update"),
